c67f0c4f3308d267f2ebbd54b27a05897c6838aa
Programer6 <delroyprithvi@gmail.com>
2025-11-01 11:16:32 +0100

    fixing typo

--- app/main.js
+++ app/main.js
@@ -39,7 +39,7 @@ const singal_dictionary = {
 if (fileContent.length !== 0) {
   for (let i = 0; i < fileContent.length; i++) {
     const ch = fileContent[i];
-    if (ch in dictionarhay) {
+    if (ch in dictionaray) {
       console.log(`${singal_dictionary[ch]} ${ch} null`);
     } else {
       console.error(`Error: Unexpected character: ${ch}`);
